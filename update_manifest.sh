a8a53a09bb4712280d1710df8c35984786c4b40c
PoiBoy <josephbalea34@gmail.com>
2026-05-25 09:04:15 -0400

    Suppression de cobblemon-battle-extras pour fix le crash

--- update_manifest.sh
+++ update_manifest.sh
@@ -25,7 +25,18 @@ for modpack_dir in modpacks/*/; do
   "version": "1.0.0",
   "files": [' > "$MANIFEST_FILE"
 
-  FIRST_FILE=true
+  # --- AJOUT MANUEL DES FICHIERS EXTERNES (RELEASES) ---
+  if [ "$MODPACK_NAME" = "Cobblemon" ]; then
+    echo '    {
+      "name": "Cobblemon-fabric-1.7.3+1.21.1.jar",
+      "url": "https://github.com/PoiBoy34/Launcher_minecraft/releases/download/METTRE_LE_NOM_DE_TA_RELEASE/Cobblemon-fabric-1.7.3+1.21.1.jar",
+      "sha1": "METTRE_LE_VRAI_HASH_ICI"
+    }' >> "$MANIFEST_FILE"
+    FIRST_FILE=false # Le premier fichier est déjà écrit
+  else
+    FIRST_FILE=true
+  fi
+  # -----------------------------------------------------
   
   # Option bash pour éviter les erreurs si le dossier est vide
   shopt -s nullglob
